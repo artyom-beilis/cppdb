f4b7bff900fbbc3c6e52ccf3f3552b286fade54a
artyom-beilis <artyom-beilis@831565cc-8e21-4ad5-8334-98019c97bd36>
2010-11-21 15:56:57 +0000

    Implemented connection pool - not tested yet

---
+++ cppdb/trunk/pool.cpp
@@ -0,0 +1,153 @@
+#include "pool.h"
+#include "backend.h"
+#include "utils.h"
+#include "driver_manager.h"
+
+#include <stdlib.h>
+
+namespace cppdb {
+
+	struct pool::data {};
+
+	pool::pool(std::string const &cs) :
+		limit_(0),
+		life_time_(0),
+		size_(0)
+	{
+		connection_info inf(cs);
+		init(cs);
+	}
+	pool::pool(connection_info const &cs) :
+		limit_(0),
+		life_time_(0),
+		size_(0)
+	{
+		init(cs);
+	}
+		
+	void pool::init(connection_info const &ci)
+	{
+		ci_ = ci;
+		
+		size_ = 0;
+		limit_ = atoi(ci_.get("@pool","16").c_str());
+		life_time_ = atoi(ci_.get("@pool_max_idle","600").c_str());
+	}
+	
+	pool::~pool()
+	{
+	}
+
+	ref_ptr<backend::connection> pool::open()
+	{
+		if(limit_ == 0)
+			return driver_manager::instance().connect(ci_);
+
+		ref_ptr<backend::connection> p = get();
+
+		if(!p) {
+			p=driver_manager::instance().connect(ci_);
+		}
+		p->recycle_pool(this);
+		return p;
+	}
+
+	// this is thread safe member function
+	ref_ptr<backend::connection> pool::get()
+	{
+		if(limit_ == 0)
+			return 0;
+		ref_ptr<backend::connection> c;
+		pool_type garbage;
+		time_t now = time(0);
+		{
+			mutex::guard l(lock_);
+			// Nothing there should throw so it is safe
+			pool_type::iterator p = pool_.begin(),tmp;
+			while(p!=pool_.end()) {
+				if(p->last_used + life_time_ < now) {
+					tmp=p;
+					p++;
+					garbage.splice(garbage.begin(),pool_,tmp);
+					size_ --;
+				}
+				else {
+					// all is sorted by time
+					break;
+				}
+			}
+			if(!pool_.empty()) {
+				c = pool_.back().conn;
+				pool_.pop_back();
+				size_ --;
+			}
+		}
+		return c;
+	}
+	
+	// this is thread safe member function
+	void pool::put(backend::connection *c_in)
+	{
+		std::auto_ptr<backend::connection> c(c_in);
+		if(limit_ == 0)
+			return;
+		pool_type garbage;
+		time_t now = time(0);
+		{
+			mutex::guard l(lock_);
+			// under lock do all very fast
+			if(c.get()) {
+				pool_.push_back(entry());
+				pool_.back().last_used = now;
+				pool_.back().conn = c.release();
+				size_ ++;
+			}
+			
+			// Nothing there should throw so it is safe
+			
+			pool_type::iterator p = pool_.begin(),tmp;
+			while(p!=pool_.end()) {
+				if(p->last_used + life_time_ < now) {
+					tmp=p;
+					p++;
+					garbage.splice(garbage.begin(),pool_,tmp);
+					size_ --;
+				}
+				else {
+					// all is sorted by time
+					break;
+				}
+			}
+			// can be at most 1 entry bigger then limit
+			if(size_ > limit_) {
+				garbage.splice(garbage.begin(),pool_,pool_.begin());
+				size_--;
+			}
+		}
+	}
+	// this is thread safe member function
+	void pool::gc()
+	{
+		put(0);
+	}
+
+	namespace backend {
+		void connection::dispose(connection *c)
+		{
+			if(!c)
+				return;
+			pool *p = c->pool_;
+			c->pool_ = 0;
+			if(p) {
+				p->put(c);
+			}
+			else
+				delete c;
+		}
+	}
+	
+	// mutex protected end
+}
+
+
+
--- cppdb/trunk/pool.h
+++ cppdb/trunk/pool.h
@@ -1,179 +1,56 @@
 #ifndef CPPDB_POOL_H
 #define CPPDB_POOL_H
-#include "backend.h"
+#include "ref_ptr.h"
 #include "mutex.h"
+#include "utils.h"
+#include <memory>
+#include <list>
+
 
 namespace cppdb {
+	class connection_info;
+	namespace backend {
+		class connection;
+	}
 
 	class pool {
 		pool(pool const &);
 		void operator=(pool const &);
 	public:
-		pool(std::string const &connection_string) : 
-			limit_(0),
-			size_(0), 
-			life_time_(0),
-			connection_string_(connection_string)
-		{
-			std::map<std::string,std::string> properties;
-			std::string driver;
-			parse_connection_string(connection_string,driver,properties);
-			std::map<std::string,std::string>::const_iterator p;
-			p = properties.find("cppdb_pool");
-			bool use_pool = true;
-			int pool_size = 16;
-			int idle_time = 600;
-			if(p!=properties.end()) {
-				if(p->second=="off") {
-					return;
-				}
-				else if(p->second=="on") {
-				}
-				else {
-					throw cppdb_error("cppdb::pool pool property should be either on or off");
-				}
-				p=properties.find("cppdb_pool_size");
-				if(p!=properties.end()) {
-					pool_size = atoi(p->second());
-				}
-				p=properties.find("cppdb_pool_max_idle");
-				if(p!=properties.end()) {
-					idle_time = atoi(p->second());
-				}
-			}
-			if(use_pool && pool_size > 0) {
-				init(pool_size,idle_time);
-			}
-		}
+		pool(std::string const &connection_string);
+		pool(connection_info const &ci);
+		~pool();
 
-		void init(size_t limit,int life_time) 
-		{
-			limit_ = limit;
-			size_ = 0;
-			life_time_ = life_time;
-		}
-		
-		~pool()
-		{
-			clear_pool(pool_);
-		}
+		ref_ptr<backend::connection> open();
+		void gc();
+		void put(backend::connection *c_in);
 
-		backend::connection *open()
-		{
-			backend::connection *p = get();
-			if(p)
-				return p;
-			return driver_manager::instance().connect(connection_string_);
-		}
-
-		// this is thread safe member function
-		backend::connection *get()
-		{
-			if(limit_ == 0)
-				return 0;
-			std::auto_ptr<backend::connection> c;
-			pool_type garbage;
-			time_t now = time(0);
-			{
-				mutex::guard l(lock_);
-				// Nothing there should throw so it is safe
-				pool_type::iterator p = pool_.begin(),tmp;
-				while(p!=pool_.end()) {
-					if(p->last_used + life_time_ < now) {
-						tmp=p;
-						p++;
-						garbage.splice(garbage.begin(),pool_,tmp);
-						size_ --;
-					}
-					else {
-						// all is sorted by time
-						break;
-					}
-				}
-				if(!pool_.empty()) {
-					c.reset(pool_.back().conn);
-					pool_.pop_back();
-					size_ --;
-				}
-			}
-			clear_pool(garbage);
-			return c.release();
-		}
-		
-		// this is thread safe member function
-		void put(backend::connection *c_in)
-		{
-			std::auto_ptr<backend::connection> c(c_in);
-			if(limit_ == 0)
-				return;
-			pool_type garbage;
-			time_t now = time(0);
-			{
-				mutex::guard l(lock_);
-				// under lock do all very fast
-				if(c.get()) {
-					pool_.push_back(entry());
-					pool_.back().last_used = now;
-					pool_.back().conn = c.release();
-					size_ ++;
-				}
-				
-				// Nothing there should throw so it is safe
-				
-				pool_type::iterator p = pool_.begin(),tmp;
-				while(p!=pool_.end()) {
-					if(p->last_used + life_time_ < now) {
-						tmp=p;
-						p++;
-						garbage.splice(garbage.begin(),pool_,tmp);
-						size_ --;
-					}
-					else {
-						// all is sorted by time
-						break;
-					}
-				}
-				// can be at most 1 entry bigger then limit
-				if(size_ > limit_) {
-					garbage.splice(time_out.begin(),pool_.begin());
-					size_--;
-				}
-			}
-			clear_pool(garbage);
-		}
-		// this is thread safe member function
-		void gc()
-		{
-			put(0);
-		}
 	private:
-		static void clear_pool(pool_type &pool)
-		{
-			pool_type::iterator p;
-			for(p=pool.begin();p!=pool.end();++p) 
-			{
-				delete p->conn;
-				p->conn = 0;
-			}
-			pool.clear();
-		}
+		ref_ptr<backend::connection> get();
+		void init(connection_info const &ci);
+
+		struct data;
+		std::auto_ptr<data> d;
+		
 		struct entry {
-			entry() : conn(0),last_used(0) {}
-			backend::connection *conn;
+			entry() : last_used(0) {}
+			ref_ptr<backend::connection> conn;
 			time_t last_used;
 		};
-		
+
+		typedef std::list<entry> pool_type;
 		// non-mutable members
 		
 		size_t limit_;
 		int life_time_;
+		connection_info ci_;
 		
 		// mutex protected begin
 		mutex lock_;
 		size_t size_;
-		typedef std::list<entry> pool_type;
 		pool_type pool_;
 		// mutex protected end
+		
 	};
 }
 
