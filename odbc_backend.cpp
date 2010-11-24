eb223babfaea8259214227051898c25d4b90b4f8
Artyom Beilis <14816918+artyom-beilis@users.noreply.github.com>
2010-11-24 21:40:00 +0000

    Now driver builds (not runs yet)

--- odbc_backend.cpp
+++ odbc_backend.cpp
@@ -1,10 +1,16 @@
 #include "backend.h"
+#include "utils.h"
+#include <list>
+#include <vector>
+#include <sstream>
+#include <limits>
+#include <iomanip>
+#include <string.h>
 
 #if defined(_WIN32) || defined(__WIN32) || defined(WIN32) || defined(__CYGWIN__)
 #include <windows.h>
 #endif
 #include <sqlext.h>
-
 
 namespace cppdb {
 
@@ -27,17 +33,22 @@ namespace cppdb {
 
 		std::string widen(std::string const &s)
 		{
-			throw not_supported_by_backend();
+			throw not_supported_by_backend("not supported for now");
 		}
 
 		std::string widen(char const *b,char const *e)
 		{
-			throw not_supported_by_backend();
+			throw not_supported_by_backend("not supported for now");
 		}
 
 		std::string narrower(std::string const &wide)
 		{
-			throw not_supported_by_backend();
+			throw not_supported_by_backend("not supported for now");
+		}
+		
+		std::basic_string<SQLWCHAR> tosqlwide(std::string const &n)
+		{
+			throw not_supported_by_backend("not supported for now");
 		}
 
 
@@ -47,12 +58,6 @@ namespace cppdb {
 			typedef std::vector<cell_type> row_type;
 			typedef std::list<row_type> rows_type;
 			
-			typedef enum {
-				last_row_reached,
-				next_row_exists,
-				next_row_unknown
-			} next_row;
-
 			virtual next_row has_next()
 			{
 				rows_type::iterator p=current_;
@@ -63,8 +68,10 @@ namespace cppdb {
 			}
 			virtual bool next() 
 			{
-				if(started_ == false)
+				if(started_ == false) {
 					current_ = rows_.begin();
+					started_ = true;
+				}
 				else if(current_!=rows_.end()) {
 					++current_;
 				}
@@ -86,47 +93,47 @@ namespace cppdb {
 			}
 			virtual bool fetch(int col,short &v)
 			{
-				return  do_feth(col,v);	
+				return  do_fetch(col,v);	
 			}
 			virtual bool fetch(int col,unsigned short &v)
 			{
-				return  do_feth(col,v);	
+				return  do_fetch(col,v);	
 			}
 			virtual bool fetch(int col,int &v)
 			{
-				return  do_feth(col,v);	
+				return  do_fetch(col,v);	
 			}
 			virtual bool fetch(int col,unsigned &v)
 			{
-				return  do_feth(col,v);	
+				return  do_fetch(col,v);	
 			}
 			virtual bool fetch(int col,long &v)
 			{
-				return  do_feth(col,v);	
+				return  do_fetch(col,v);	
 			}
 			virtual bool fetch(int col,unsigned long &v)
 			{
-				return  do_feth(col,v);	
+				return  do_fetch(col,v);	
 			}
 			virtual bool fetch(int col,long long &v)
 			{
-				return  do_feth(col,v);	
+				return  do_fetch(col,v);	
 			}
 			virtual bool fetch(int col,unsigned long long &v)
 			{
-				return  do_feth(col,v);	
+				return  do_fetch(col,v);	
 			}
 			virtual bool fetch(int col,float &v)
 			{
-				return  do_feth(col,v);	
+				return  do_fetch(col,v);	
 			}
 			virtual bool fetch(int col,double &v)
 			{
-				return  do_feth(col,v);	
+				return  do_fetch(col,v);	
 			}
 			virtual bool fetch(int col,long double &v)
 			{
-				return  do_feth(col,v);	
+				return  do_fetch(col,v);	
 			}
 			virtual bool fetch(int col,std::string &v)
 			{
@@ -155,15 +162,17 @@ namespace cppdb {
 			}
 			virtual int cols()
 			{
-				throw not_supported_by_backend();
+				if(rows_.begin()!=rows_.end())
+					return rows_.begin()->size();
+				return 0;
 			}
 			virtual int name_to_column(std::string const &) 
 			{
-				throw not_supported_by_backend();
+				throw not_supported_by_backend("not supported for now");
 			}
 			virtual std::string column_to_name(int) 
 			{
-				throw not_supported_by_backend();
+				throw not_supported_by_backend("not supported for now");
 			}
 			
 			result(rows_type &rows)
@@ -174,7 +183,7 @@ namespace cppdb {
 			}
 			cell_type &at(int col)
 			{
-				if(current_!=rows_.end() && col >= 0 && col <current_->size())
+				if(current_!=rows_.end() && col >= 0 && col <int(current_->size()))
 					return current_->at(col);
 				throw invalid_column();
 			}
@@ -187,6 +196,108 @@ namespace cppdb {
 		class statements_cache;
 
 		class statement : public backend::statement {
+			struct parameter {
+				parameter() : 
+					null(true),
+					ctype(SQL_C_CHAR),
+					sqltype(SQL_C_NUMERIC)
+				{
+				}
+				void set(char const *b,char const *e,bool wide,int sqt)
+				{
+					if(!wide) {
+						value.assign(b,e-b);
+						null=false;
+						ctype=SQL_C_CHAR;
+						sqltype = sqt;
+					}
+					else {
+						std::string tmp = widen(b,e);
+						null=false;
+						ctype=SQL_C_WCHAR;
+						sqltype = sqt;
+					}
+				}
+				void set(std::string &s,bool wide,int sqt)
+				{
+					if(!wide) {
+						value.swap(s);
+						null=false;
+						ctype=SQL_C_CHAR;
+						sqltype = sqt;
+					}
+					else {
+						std::string tmp = widen(s);
+						null=false;
+						ctype=SQL_C_WCHAR;
+						sqltype = sqt;
+					}
+				}
+				void set(char const *b,char const *e,bool wide=false)
+				{
+					if(!wide) {
+						set(b,e,false,SQL_LONGVARCHAR);
+					}
+					else {
+						set(b,e,true,SQL_WLONGVARCHAR);
+					}
+				}
+				void set(std::tm const &v,bool wide=false)
+				{
+					std::string s = format_time(v);
+					set(s,wide,SQL_C_TIMESTAMP);
+				}
+				template<typename T>
+				void set(T v,bool wide=false)
+				{
+					std::ostringstream ss;
+					ss.imbue(std::locale::classic());
+					if(!std::numeric_limits<T>::is_integer)
+						ss << std::setprecision(std::numeric_limits<T>::digits10+1);
+					ss << v;
+					std::string sv=ss.str();
+					if(std::numeric_limits<T>::is_integer)
+						set(sv,wide,SQL_INTEGER);
+					else
+						set(sv,wide,SQL_DOUBLE);
+				}
+				void bind(int col,SQLHSTMT stmt)
+				{
+					int r;
+					if(null) {
+						SQLLEN len = SQL_NULL_DATA;
+						r = SQLBindParameter(	stmt,
+									col,
+									SQL_PARAM_INPUT,
+									SQL_C_CHAR, // C type C_CHAR or C_WCHAR
+									SQL_NUMERIC, // for null
+									value.size(), // COLUMNSIZE
+									0, //  Presision
+									0, // string
+									0, // size
+									&len);
+					}
+					else {
+						r = SQLBindParameter(	stmt,
+									col,
+									SQL_PARAM_INPUT,
+									ctype, // C type C_CHAR or C_WCHAR
+									sqltype,
+									value.size(), // COLUMNSIZE
+									0, //  Presision
+									(SQLCHAR*)value.c_str(), // string
+									value.size(),
+									0);
+					}
+
+					check_odbc_error(r,stmt,SQL_HANDLE_STMT);
+				}
+
+				std::string value;
+				bool null;
+				SQLSMALLINT ctype;
+				SQLSMALLINT sqltype;
+			};
 		public:
 			// Begin of API
 			virtual void reset()
@@ -198,7 +309,7 @@ namespace cppdb {
 			{
 				if(col < 0)
 					throw invalid_placeholder();
-				if(params_.size() < col)
+				if(params_.size() < size_t(col))
 					params_.resize(col);
 				return params_[col - 1];
 			}
@@ -226,15 +337,7 @@ namespace cppdb {
 			{
 				std::ostringstream ss;
 				ss << in.rdbuf();
-				if(!wide_) {
-					std::string tmp=ss.str();
-					bind(col,tmp,SQL_LONGVARCHAR);
-				}
-				else {
-					sqlwstring tmp=tosqlwide(ss.str());
-					bind(col,tmp,SQL_WLONGVARCHAR);
-				}
-					
+				bind(col,ss.str());
 			}
 			template<typename T>
 			void do_bind_num(int col,T v)
@@ -280,7 +383,7 @@ namespace cppdb {
 			void bind_all()
 			{
 				for(unsigned i=0;i<params_.size();i++) {
-					params_.bind(stat_,i+1);
+					params_[i].bind(i+1,stmt_);
 				}
 
 			}
@@ -297,13 +400,15 @@ namespace cppdb {
 			}
 			virtual result *query()
 			{
+				bind_all();
+				int r=SQLExecute(stmt_);
+				check_error(r);
 				char buf[1024];
 				result::rows_type rows;
 				result::row_type row;
 				std::string value;
 				bool is_null = false;
-				int r;
-				while((r=SQLFetch(stmt_))!=SQL_NO_DATA) {
+				while((r=SQLFetch(stmt_))==SQL_SUCCESS || r==SQL_SUCCESS_WITH_INFO) {
 					row.reserve(100);
 					for(int col=1;;col++) {
 						SQLLEN len = 0;
@@ -328,7 +433,7 @@ namespace cppdb {
 								}
 
 							}
-							if(len > sizeof(buf)) {
+							else if(len > SQLLEN(sizeof(buf))) {
 								if(!wide_)
 									value.append(buf,sizeof(buf)-1);
 								else
@@ -357,13 +462,17 @@ namespace cppdb {
 						}
 						row.back().second.swap(value);
 					}
-					rows.push_back(row_type);
+					rows.push_back(result::row_type());
 					rows.back().swap(row);
+				}
+				if(r!=SQL_NO_DATA) {
+					check_error(r);
 				}
 				return new result(rows);
 			}
 			virtual void exec()
 			{
+				bind_all();
 				int r=SQLExecute(stmt_);
 				check_error(r);
 			}
@@ -381,12 +490,12 @@ namespace cppdb {
 					if(wide_) 
     						r = SQLPrepareW(
 							stmt_,
-							tosqlwide(query).c_str(),
+							(SQLWCHAR*)tosqlwide(query_).c_str(),
 							SQL_NTS);
 					else
     						r = SQLPrepareA(
 							stmt_,
-							query.c_str(),
+							(SQLCHAR*)query_.c_str(),
 							SQL_NTS);
 					check_error(r);
 					params_.reserve(100);
@@ -406,107 +515,6 @@ namespace cppdb {
 				check_odbc_error(code,stmt_,SQL_HANDLE_STMT);
 			}
 
-			struct parameter {
-				parameter() : 
-					null(true),
-					ctype(SQL_C_CHAR),
-					sqltype(SQL_C_NUMERIC)
-				{
-				}
-				void set(char const *b,char const *e,bool wide,int sqt)
-				{
-					if(!wide) {
-						value.assign(b,e-b);
-						null=false;
-						ctype=SQL_C_CHAR;
-						sqltype = sqt;
-					}
-					else {
-						std::string tmp = widen(b,e-b);
-						null=false;
-						ctype=SQL_C_WCHAR;
-						sqltype = sqt;
-					}
-				}
-				void set(std::string &s,bool wide,int sqt)
-				{
-					if(!wide) {
-						value.swap(s);
-						null=false;
-						ctype=SQL_C_CHAR;
-						sqltype = sqt;
-					}
-					else {
-						std::string tmp = widen(s);
-						null=false;
-						ctype=SQL_C_WCHAR;
-						sqltype = sqt;
-					}
-				}
-				void set(char const *b,char const *e,bool wide=false)
-				{
-					if(!wide) {
-						set(b,e,false,SQL_C_LONGVARCHAR);
-					}
-					else {
-						set(b,e,true,SQL_C_WLONGVARCHAR);
-					}
-				}
-				void set(std::tm const &v,bool wide=false)
-				{
-					std::string s = format_time(v);
-					set(s,wide,SQL_C_TIMESTAMP);
-				}
-				template<typename T>
-				void set(T v,bool wide=false)
-				{
-					std::ostringstream ss;
-					ss.imbue(std::locale::classic());
-					if(!std::numeric_limits<T>::is_integer)
-						ss << std::setprecision(std::numeric_limits<T>::digits10+1);
-					ss << v;
-					std::string sv=ss.str();
-					if(std::numeric_limits<T>::is_integer)
-						set(sv,wide,SQL_INTEGER);
-					else
-						set(sv,wide,SQL_DOUBLE);
-				}
-				void bind(int col,SQLHSTMT stmt)
-				{
-					int r;
-					if(null) {
-						r = SQLBindParameter(	stmt,
-									col,
-									SQL_PARAM_INPUT,
-									SQL_C_CHAR, // C type C_CHAR or C_WCHAR
-									SQL_NUMERIC, // for null
-									value.size(), // COLUMNSIZE
-									0, //  Presision
-									0, // string
-									0, // size
-									SQL_NULL_VAL_SHOULD_BE_FIX_ME); // FIXME
-					}
-					else {
-						r = SQLBindParameter(	stmt,
-									col,
-									SQL_PARAM_INPUT,
-									ctype, // C type C_CHAR or C_WCHAR
-									sqltype,
-									value.size(), // COLUMNSIZE
-									0, //  Presision
-									(SQLCHAR*)value.c_str(), // string
-									value.size(),
-									0);
-					}
-
-					check_odbc_error(r,stmt,SQL_HANDLE_STMT);
-				}
-
-				std::string value;
-				bool null;
-				SQLSMALLINT ctype;
-				SQLSMALLINT sqltype;
-			};
 
 			SQLHSTMT stmt_;
 			bool wide_;
@@ -524,7 +532,7 @@ namespace cppdb {
 				
 				if(utf_mode == "narrow")
 					wide_ = false;
-				else if(utf_mode = "wide");
+				else if(utf_mode == "wide")
 					wide_ = true;
 				else
 					throw cppdb_error("cppdb::odbc:: @utf property can be either 'narrow' or 'wide'");
@@ -540,19 +548,21 @@ namespace cppdb {
 					}
 					env_created = true;
 					r = SQLSetEnvAttr(env_,SQL_ATTR_ODBC_VERSION,(SQLPOINTER)SQL_OV_ODBC3, 0);
-					check_odbc_error(r,SQL_HANDLE_ENV,env_);
+					check_odbc_error(r,env_,SQL_HANDLE_ENV);
 					r = SQLAllocHandle(SQL_HANDLE_DBC,env_,&dbc_);
-					check_odbc_error(r,SQL_HANDLE_ENV,env_);
+					check_odbc_error(r,env_,SQL_HANDLE_ENV);
 					dbc_created = true;
 					if(ci.get("@utf","narrow")=="wide") {
-						r = SQLDriverConnectW(dbc_,0,tosqlwide(conn_str(ci)).c_str(),
+						r = SQLDriverConnectW(dbc_,0,
+									(SQLWCHAR*)tosqlwide(conn_str(ci)).c_str(),
 									SQL_NTS,0,0,0,SQL_DRIVER_COMPLETE);
 					}
 					else {
-						r = SQLDriverConnectA(dbc_,0,conn_str(ci).c_str(),
+						r = SQLDriverConnectA(dbc_,0,
+									(SQLCHAR*)conn_str(ci).c_str(),
 									SQL_NTS,0,0,0,SQL_DRIVER_COMPLETE);
 					}
-					check_odbc_error(r,SQL_HANDLE_DBC,dbc_);
+					check_odbc_error(r,dbc_,SQL_HANDLE_DBC);
 					set_autocommit(true);
 				}
 				catch(...) {
@@ -573,10 +583,10 @@ namespace cppdb {
 				for(p=ci.properties.begin();p!=ci.properties.end();p++) {
 					if(p->first.empty() || p->first[0]=='@')
 						continue;
-					pq_str+=p->first;
-					pq_str+="=";
-					pq_str+=p->second;
-					pq_str+=";";
+					str+=p->first;
+					str+="=";
+					str+=p->second;
+					str+=";";
 				}
 				return str;
 			}
@@ -596,7 +606,7 @@ namespace cppdb {
 			virtual void commit()
 			{
 				SQLRETURN r = SQLEndTran(SQL_HANDLE_DBC,dbc_,SQL_COMMIT);
-				check_odbc_error(r,SQL_HANDLE_DBC,dbc_);
+				check_odbc_error(r,dbc_,SQL_HANDLE_DBC);
 				set_autocommit(true);
 			}
 
@@ -604,7 +614,7 @@ namespace cppdb {
 			{
 				try {
 					SQLRETURN r = SQLEndTran(SQL_HANDLE_DBC,dbc_,SQL_ROLLBACK);
-					check_odbc_error(r,SQL_HANDLE_DBC,dbc_);
+					check_odbc_error(r,dbc_,SQL_HANDLE_DBC);
 				}
 				catch(...) {}
 				try {
@@ -614,7 +624,7 @@ namespace cppdb {
 			}
 			virtual statement *real_prepare(std::string const &q)
 			{
-				return new statement(q,dbc_);
+				return new statement(q,dbc_,wide_);
 			}
 			virtual std::string escape(std::string const &s)
 			{
@@ -647,6 +657,7 @@ namespace cppdb {
 		private:
 			SQLHENV env_;
 			SQLHDBC dbc_;
+			bool wide_;
 		};
 
 
@@ -659,4 +670,3 @@ extern "C" {
 		return new cppdb::odbc_backend::connection(cs);
 	}
 }
-#endif
