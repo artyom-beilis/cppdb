fb160f718c0a18170ab205f42f2dbcc2aa723743
artyom-beilis <artyom-beilis@831565cc-8e21-4ad5-8334-98019c97bd36>
2010-11-25 15:03:42 +0000

    - Windows Support - Utf-16/wide-odbc support

--- cppdb/trunk/odbc_backend.cpp
+++ cppdb/trunk/odbc_backend.cpp
@@ -13,6 +13,222 @@
 #include <sqlext.h>
 
 namespace cppdb {
+namespace odbc_backend {
+
+typedef unsigned odbc_u32;
+typedef unsigned short odbc_u16;
+
+int assert_on_unsigned_is_32[sizeof(unsigned) == 4 ? 1 : -1];
+int assert_on_unsigned_short_is_16[sizeof(unsigned short) == 2 ? 1 : -1];
+
+
+
+namespace utf {
+	static const odbc_u32 illegal = 0xFFFFFFFFu;
+	inline bool valid(odbc_u32 v)
+	{
+		if(v>0x10FFFF)
+			return false;
+		if(0xD800 <=v && v<= 0xDFFF) // surragates
+			return false;
+		return true;
+	}
+}
+
+namespace utf8 {
+	// See RFC 3629
+	// Based on: http://www.w3.org/International/questions/qa-forms-utf-8
+	template<typename Iterator>
+	odbc_u32 next(Iterator &p,Iterator e)
+	{
+		unsigned char c=*p++;
+		unsigned char seq0,seq1=0,seq2=0,seq3=0;
+		seq0=c;
+		int len=1;
+		if((c & 0xC0) == 0xC0) {
+			if(p==e)
+				return utf::illegal;
+			seq1=*p++;
+			len=2;
+		}
+		if((c & 0xE0) == 0xE0) {
+			if(p==e)
+				return utf::illegal;
+			seq2=*p++;
+			len=3;
+		}
+		if((c & 0xF0) == 0xF0) {
+			if(p==e)
+				return utf::illegal;
+			seq3=*p++;
+			len=4;
+		}
+		switch(len) {
+		case 1: // ASCII -- remove codes for HTML only
+			if(seq0 > 0x7F)
+				return utf::illegal;
+			break;
+		case 2: // non-overloading 2 bytes
+			if(0xC2 <= seq0 && seq0 <= 0xDF) {
+				if(0x80 <= seq1 && seq1<= 0xBF)
+					break;
+			}
+			return utf::illegal;
+		case 3: 
+			if(seq0==0xE0) { // exclude overloadings
+				if(0xA0 <=seq1 && seq1<= 0xBF && 0x80 <=seq2 && seq2<=0xBF)
+					break;
+			}
+			else if( (0xE1 <= seq0 && seq0 <=0xEC) || seq0==0xEE || seq0==0xEF) { // stright 3 bytes
+				if(	0x80 <=seq1 && seq1<=0xBF &&
+					0x80 <=seq2 && seq2<=0xBF)
+					break;
+			}
+			else if(seq0 == 0xED) { // exclude surrogates
+				if(	0x80 <=seq1 && seq1<=0x9F &&
+					0x80 <=seq2 && seq2<=0xBF)
+					break;
+			}
+			return utf::illegal;
+		case 4:
+			switch(seq0) {
+			case 0xF0: // planes 1-3
+				if(	0x90 <=seq1 && seq1<=0xBF &&
+					0x80 <=seq2 && seq2<=0xBF &&
+					0x80 <=seq3 && seq3<=0xBF)
+					break;
+				return utf::illegal;
+			case 0xF1: // planes 4-15
+			case 0xF2:
+			case 0xF3:
+				if(	0x80 <=seq1 && seq1<=0xBF &&
+					0x80 <=seq2 && seq2<=0xBF &&
+					0x80 <=seq3 && seq3<=0xBF)
+					break;
+				return utf::illegal;
+			case 0xF4: // pane 16
+				if(	0x80 <=seq1 && seq1<=0x8F &&
+					0x80 <=seq2 && seq2<=0xBF &&
+					0x80 <=seq3 && seq3<=0xBF)
+					break;
+				return utf::illegal;
+			default:
+				return utf::illegal;
+			}
+
+		}
+		
+		switch(len) {
+		case 1:
+			return seq0;
+		case 2:
+			return ((seq0 & 0x1F) << 6) | (seq1 & 0x3F);
+		case 3:
+			return ((seq0 & 0x0F) << 12) | ((seq1 & 0x3F) << 6) | (seq2 & 0x3F)  ;
+		case 4:
+			return ((seq0 & 0x07) << 18) | ((seq1 & 0x3F) << 12) | ((seq2 & 0x3F) << 6) | (seq3 & 0x3F) ;
+		}
+
+		return utf::illegal;
+	} // valid
+
+
+	struct seq {
+		char c[4];
+		unsigned len;
+	};
+	inline seq encode(odbc_u32 value)
+	{
+		seq out={ {0} };
+		if(value <=0x7F) {
+			out.c[0]=value;
+			out.len=1;
+		}
+		else if(value <=0x7FF) {
+			out.c[0]=(value >> 6) | 0xC0;
+			out.c[1]=(value & 0x3F) | 0x80;
+			out.len=2;
+		}
+		else if(value <=0xFFFF) {
+			out.c[0]=(value >> 12) | 0xE0;
+			out.c[1]=((value >> 6) & 0x3F) | 0x80;
+			out.c[2]=(value & 0x3F) | 0x80;
+			out.len=3;
+		}
+		else {
+			out.c[0]=(value >> 18) | 0xF0;
+			out.c[1]=((value >> 12) & 0x3F) | 0x80;
+			out.c[2]=((value >> 6) & 0x3F) | 0x80;
+			out.c[3]=(value & 0x3F) | 0x80;
+			out.len=4;
+		}
+		return out;
+	}
+} // namespace utf8
+
+
+namespace utf16 {
+
+	// See RFC 2781
+	inline bool is_first_surrogate(odbc_u16 x)
+	{
+		return 0xD800 <=x && x<= 0xDBFF;
+	}
+	inline bool is_second_surrogate(odbc_u16 x)
+	{
+		return 0xDC00 <=x && x<= 0xDFFF;
+	}
+	inline odbc_u32 combine_surrogate(odbc_u16 w1,odbc_u16 w2)
+	{
+		return ((odbc_u32(w1 & 0x3FF) << 10) | (w2 & 0x3FF)) + 0x10000;
+	}
+
+	template<typename It>
+	inline odbc_u32 next(It &current,It last)
+	{
+		odbc_u16 w1=*current++;
+		if(w1 < 0xD800 || 0xDFFF < w1) {
+			return w1;
+		}
+		if(w1 > 0xDBFF)
+			return utf::illegal;
+		if(current==last)
+			return utf::illegal;
+		odbc_u16 w2=*current++;
+		if(w2 < 0xDC00 || 0xDFFF < w2)
+			return utf::illegal;
+		return combine_surrogate(w1,w2);
+	}
+	inline int width(odbc_u32 u)
+	{
+		return u>=0x100000 ? 2 : 1;
+	}
+	struct seq {
+		odbc_u16 c[2];
+		unsigned len;
+	};
+	inline seq encode(odbc_u32 u)
+	{
+		seq out={ {0} };
+		if(u<=0xFFFF) {
+			out.c[0]=u;
+			out.len=1;
+		}
+		else {
+			u-=0x10000;
+			out.c[0]=0xD800 | (u>>10);
+			out.c[1]=0xDC00 | (u & 0x3FF);
+			out.len=2;
+		}
+		return out;
+	}
+} // utf16;
+
+} // odbc_backend
+} // cppdb
+
+
+namespace cppdb {
 
 	class connection_info;
 	class pool;
@@ -23,32 +239,86 @@ namespace cppdb {
 		{
 			if(SQL_SUCCEEDED(error))
 				return;
-			SQLCHAR msg[SQL_MAX_MESSAGE_LENGTH + 2] = {0};
-			SQLCHAR stat[SQL_SQLSTATE_SIZE + 1] = {0};
-			SQLINTEGER err;
-			SQLSMALLINT len;
-			SQLGetDiagRec(type,h,1,stat,&err,msg,sizeof(msg),&len);
-			throw cppdb_error("cppdb::odbc::Failed with error `" + std::string((char *)msg)+"'");
-		}
+			std::string error_message;
+			int rec=1,r;
+			for(;;){
+				SQLCHAR msg[SQL_MAX_MESSAGE_LENGTH + 2] = {0};
+				SQLCHAR stat[SQL_SQLSTATE_SIZE + 1] = {0};
+				SQLINTEGER err;
+				SQLSMALLINT len;
+				r = SQLGetDiagRec(type,h,rec,stat,&err,msg,sizeof(msg),&len);
+				rec++;
+				if(r==SQL_SUCCESS || r==SQL_SUCCESS_WITH_INFO) {
+					if(!error_message.empty())
+						error_message+='\n';
+					error_message +=(char *)msg;
+				}
+				else 
+					break;
 
-		std::string widen(std::string const &s)
-		{
-			throw not_supported_by_backend("not supported for now");
+			} 
+			throw cppdb_error("cppdb::odbc::Failed with error `" + error_message +"'");
 		}
 
 		std::string widen(char const *b,char const *e)
 		{
-			throw not_supported_by_backend("not supported for now");
+			std::string result;
+			result.reserve((e-b)*2);
+			odbc_u32 code_point = 0;
+			while(b < e && (code_point = utf8::next(b,e))!=utf::illegal) {
+				utf16::seq sq = utf16::encode(code_point);
+				char *str = (char *)sq.c;
+				result.append(str,sq.len * 2);
+			}
+			if(b!=e || code_point == utf::illegal) {
+				throw cppdb_error("cppdb::odbc invalid UTF-8 input");
+			}
+			return result;
 		}
+		
+		std::string widen(std::string const &s)
+		{
+			return widen(s.c_str(),s.c_str()+s.size());
+		}
+
 
 		std::string narrower(std::string const &wide)
 		{
-			throw not_supported_by_backend("not supported for now");
+			if(wide.size() % 2 != 0) {
+				throw cppdb_error("cppdb::odbc got invalid UTF-16");
+			}
+			odbc_u16 const *b = reinterpret_cast<odbc_u16 const *>(wide.c_str());
+			odbc_u16 const *e = b + wide.size() / 2;
+
+			std::string result;
+			result.reserve((e-b));
+
+			odbc_u32 code_point = 0;
+			while(b < e && (code_point = utf16::next(b,e))!=utf::illegal) {
+				utf8::seq sq = utf8::encode(code_point);
+				result.append(sq.c,sq.len);
+			}
+			if(b!=e || code_point == utf::illegal) {
+				throw cppdb_error("cppdb::odbc got invalid UTF-16");
+			}
+			return result;
 		}
 		
 		std::basic_string<SQLWCHAR> tosqlwide(std::string const &n)
 		{
-			throw not_supported_by_backend("not supported for now");
+			std::basic_string<SQLWCHAR> result;
+			char const *b=n.c_str();
+			char const *e=b+n.size();
+			result.reserve(e-b);
+			odbc_u32 code_point = 0;
+			while(b < e && (code_point = utf8::next(b,e))!=utf::illegal) {
+				utf16::seq sq = utf16::encode(code_point);
+				result.append((SQLWCHAR*)sq.c,sq.len);
+			}
+			if(b!=e || code_point == utf::illegal) {
+				throw cppdb_error("cppdb::odbc invalid UTF-8 input");
+			}
+			return result;
 		}
 
 
