edfe207af1b3cac2a51c50d15f293a21003ef27a
artyom-beilis <artyom-beilis@831565cc-8e21-4ad5-8334-98019c97bd36>
2010-12-02 15:48:55 +0000

    Added copyright notices

--- cppdb/trunk/template_header.h
+++ cppdb/trunk/template_header.h
@@ -16,21 +16,3 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 ///////////////////////////////////////////////////////////////////////////////
-#ifndef CPPDB_DEFS_H
-#define CPPDB_DEFS_H
-
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) || defined(__CYGWIN__)
-#  ifdef DLL_EXPORT
-#    ifdef CPPDB_SOURCE
-#      define CPPDB_API __declspec(dllexport)
-#    else
-#      define CPPDB_API __declspec(dllimport)
-#    endif
-#  else
-#    define CPPDB_API
-#  endif
-#else
-#  define CPPDB_API
-#endif
-
-#endif
